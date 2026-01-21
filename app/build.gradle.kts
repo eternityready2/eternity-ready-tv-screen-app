7cbcd7fa7124289cf47fda75396f9e3b2a27e302
sivefunc <sivefunc@tuta.io>
2026-01-20 20:20:04 -0400

    build: update version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "com.weternityreadymedia.eternityready.eternityreadytv"
         minSdk = 21
         targetSdk = 36
-        versionCode = 7
-        versionName = "1.3.0"
+        versionCode = 8
+        versionName = "1.3.1"
 
     }
 
